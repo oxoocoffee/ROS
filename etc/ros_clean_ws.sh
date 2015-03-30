07fa029802d51f6785af9407b7684ff39b1185aa
Robert J. Gebis <rjgebis@gmail.com>
2015-03-29 21:46:21 -0500

    Cleaned up project scripts

--- etc/ros_clean_ws.sh
+++ etc/ros_clean_ws.sh
@@ -2,19 +2,33 @@
 # By Robert J. Gebis (oxoocoffee)
 # rjgebis AT g mail DOT com
 
+if [ -z "$ROS_WORKSPACE" ]; then
+    echo "Need to set ROS_WORKSPACE"
+    echo "Did you \"source ROS_WS_PROJECT\etc\ros_switch_ws.sh\"?"
+    echo ""
+    exit 1
+fi
+
+echo "Cleaning $ROS_WORKSPACE"
+echo ""
+
+catkin_make clean
+
+echo ""
 echo ""
 response=""
 
 if [ $# -ne 1 ] 
    then
-       read -r -p "Are you sure? [y/N] " response
+       read -r -p "Do you want to purge log files? [y/N] " response
    else
        response=$1
 fi
 
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
    then
-       catkin_make clean
-       rm -rf ./run/*.pid ./run/logs/*
+       rosclean purge
+       echo "Logs purged from ${ROS_LOG_DIR}"
+       echo ""
 fi
 
